403f4cc70b1e9a15e8fb3abbc38cffe612015e64
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-05 00:27:12 -0500

    modificacion traductor

--- NIVEL-1/traductor.rb
+++ NIVEL-1/traductor.rb
@@ -13,17 +13,18 @@ No encontramos la traducción de hola
 Aunque este ejercicio se puede solucionar utilizando condicionales, intenta definir un hash con las traducciones (la llave sería la palabra en español y el valor la palabra en inglés) y utilizar ese hash para traducir la palabra que ingrese el usuario.
 =end
 
-hash = 
-{uno: 'one', 
-dos: 'two', 
-tres: 'three', 
-cuatro: 'four', 
-cinco: 'five', 
-seis: 'six', 
-siete: 'sevent', 
-ocho: 'eight', 
-nueve: 'nine', 
-dies: 'ten'}
+hash = {
+  uno: 'one', 
+  dos: 'two', 
+  tres: 'three', 
+  cuatro: 'four', 
+  cinco: 'five', 
+  seis: 'six', 
+  siete: 'sevent', 
+  ocho: 'eight', 
+  nueve: 'nine', 
+  dies: 'ten'
+}
 
 print 'Escribe la palabra a traducir: '
 palabra = gets.chomp.to_sym
