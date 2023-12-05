fa486bc21c1e5bfa2769d14f42a44d0ec842d5f9
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-05 00:19:04 -0500

    finalizacion ejecicio traductor

---
+++ NIVEL-1/traductor.rb
@@ -0,0 +1,37 @@
+=begin
+                Traductor
+Escribe un programa que traduzca los números del uno al diez de español a inglés. Si es cualquier otra palabra deberá imprimir "No encontramos la traducción de " seguido de la palabra que escribió el usuario. Por ejemplo:
+
+$ ruby traductor.rb
+Escribe la palabra a traducir: uno
+
+La traducción es: one
+$ ruby
+Escribe la palabra a traducir: hola
+
+No encontramos la traducción de hola
+Aunque este ejercicio se puede solucionar utilizando condicionales, intenta definir un hash con las traducciones (la llave sería la palabra en español y el valor la palabra en inglés) y utilizar ese hash para traducir la palabra que ingrese el usuario.
+=end
+
+hash = 
+{uno: 'one', 
+dos: 'two', 
+tres: 'three', 
+cuatro: 'four', 
+cinco: 'five', 
+seis: 'six', 
+siete: 'sevent', 
+ocho: 'eight', 
+nueve: 'nine', 
+dies: 'ten'}
+
+print 'Escribe la palabra a traducir: '
+palabra = gets.chomp.to_sym
+
+traducir = hash[palabra]
+puts
+if traducir
+  puts "La traducción es: #{traducir}"
+else
+  puts "No encontramos la traducción de #{palabra}"
+end
