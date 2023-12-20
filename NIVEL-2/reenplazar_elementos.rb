a5fb95909cb6982b04307a650154b372b41563fe
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-19 21:24:02 -0500

    finalizacion reenplazar_elementos.rb

---
+++ NIVEL-2/reenplazar_elementos.rb
@@ -0,0 +1,21 @@
+=begin
+              Reemplazar elementos
+Escribe un método llamado reemplazar que reciba un arreglo y reemplace todos los elementos que sean un 1 por un 0 y lo contrario, todos los elementos que sean 0 por 1.
+
+# escribe tu solución acá
+
+puts reemplazar([1, 1]).inspect # [0, 0]
+puts reemplazar([0, 0]).inspect # [1, 1]
+puts reemplazar([1, 0, 0, 1]).inspect # [0, 1, 1, 0]
+puts reemplazar([1, 0, 2]).inspect # [0, 1, 2]
+Ver solución
+=end
+
+def reemplazar(arr)
+  arr.map { |elemento| elemento == 1 ? 0 : (elemento == 0 ? 1 : elemento) }
+end
+
+puts reemplazar([1, 1]).inspect # [0, 0]
+puts reemplazar([0, 0]).inspect # [1, 1]
+puts reemplazar([1, 0, 0, 1]).inspect # [0, 1, 1, 0]
+puts reemplazar([1, 0, 2]).inspect # [0, 1, 2]
