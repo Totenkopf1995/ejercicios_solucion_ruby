c915929bbabab367d9b7e084632552581e004271
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-19 19:18:00 -0500

    terminacion likes inicio contar_elementos

---
+++ NIVEL-2/contar_elementos.rb
@@ -0,0 +1,27 @@
+=begin
+               Contar elementos
+Escribe un método contar que reciba un arreglo y retorne la cantidad de veces que aparece el número 5.
+
+# escribe tu solución acá
+
+puts contar([1, 5, 5, 1]) # 2
+puts contar([2, 6, "5"]) # 0
+puts contar([]) # 0
+Ver solución
+
+Si quieres agregarle más dificultad modifica el método para que reciba un argumento adicional y retorne la cantidad de veces que aparece ese argumento en el arreglo:
+
+# escribe tu solución acá
+
+puts contar([1, 5, 5, 1], 5) # 2
+puts contar([2, 6, "5"], "5") # 1
+puts contar([1, "hola", 3, "mundo"], 5) # 0
+=end
+
+def contar
+  
+end
+
+puts contar([1, 5, 5, 1]) # 2
+puts contar([2, 6, "5"]) # 0
+puts contar([]) # 0
--- NIVEL-2/numero_likes.rb
+++ NIVEL-2/numero_likes.rb
@@ -25,9 +25,13 @@ def numero_de_likes(num)
   end
 end
 
+puts
+
 puts numero_de_likes(778) # "778"
 puts numero_de_likes(1000) # "1K"
 puts numero_de_likes(1999) # "1K"
 puts numero_de_likes(34_567) # "34K"
 puts numero_de_likes(999_999) # "999K"
 puts numero_de_likes(7_456_345) # "7M"
+
+puts
