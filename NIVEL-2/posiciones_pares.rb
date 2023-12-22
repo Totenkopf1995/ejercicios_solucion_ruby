95b58cda5f7d24e8c43289072f3836a29d950751
Totenkopf1995 <uroxacin1@hotmail.com>
2023-12-22 17:20:23 -0500

    finalizacion reenplazar_elementos.rb

---
+++ NIVEL-2/posiciones_pares.rb
@@ -0,0 +1,20 @@
+=begin
+                Posiciones pares
+Escribe un método llamado posiciones_pares que reciba un arreglo y devuelva un nuevo arreglo con los valores que se encuentran en las posiciones pares del arreglo (0, 2, 4, 6, 8, etc.)
+
+# escribe tu solución acá
+
+puts posiciones_pares([0, 1, 2, 3, 4]).inspect # [0, 2, 4]
+puts posiciones_pares(["Make", "sun", "it", "bright", "Real"]).inspect # ["Make", "it", "Real"]
+puts posiciones_pares([]).inspect # []
+Ver solución
+=end
+
+def posiciones_pares(arr)
+  arr.select.with_index { |element, index| index.even? }
+end
+
+puts posiciones_pares([0, 1, 2, 3, 4]).inspect # Output: [0, 2, 4]
+puts posiciones_pares(["Make", "sun", "it", "bright", "Real"]).inspect # Output: ["Make", "it", "Real"]
+puts posiciones_pares([]).inspect # Output: []
+
