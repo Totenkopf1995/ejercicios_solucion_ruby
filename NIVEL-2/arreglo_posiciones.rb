c6104f722bdbfec0dda0e8d48a0a93d0b3f9200b
totenkopf1995 <uroxacin1@hotmail.com>
2023-12-30 16:02:42 -0500

    finalizacion arreglo_posiciones.rb

--- NIVEL-2/arreglo_posiciones.rb
+++ NIVEL-2/arreglo_posiciones.rb
@@ -13,7 +13,11 @@ Ver Solución
 =end
 
 def posiciones_num_1(arr)
-
+  ret = []
+  arr.each_with_index do |val, i|
+    ret << i if val == 1
+  end
+  ret
 end
 
 puts posiciones_num_1([0, 1, 0, 1]).inspect # [1, 3]
